005a054507f710797a69d0af3b632f2396192c38
Hoang Dat <hoangdat@MacBook-Pro-cua-Hoang.local>
2019-04-17 23:51:49 +0700

    hash password use bcrypt

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');//khai bao nhung cai field co trong OBJ cua chung ta
 mongoose.set('useCreateIndex', true);
+var  bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
 
 var userSchema= new mongoose.Schema({
  	username:{
@@ -9,10 +11,38 @@ var userSchema= new mongoose.Schema({
  	},
  	password:{
  		type:String,
- 		require:true
+ 		required:true
 
  	}
 
  });
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
  var User = mongoose.model('User',userSchema,'users');
  module.exports = User;
